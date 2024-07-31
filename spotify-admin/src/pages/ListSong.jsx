5f2814ca981544cbfc220ca603a441f40b4c8145
Jijo Jacob <Jijojacob988@gmail.com>
2024-07-31 17:56:34 +0530

    list song function created

--- spotify-admin/src/pages/ListSong.jsx
+++ spotify-admin/src/pages/ListSong.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { url } from '../App';
+import { toast } from 'react-toastify';
 
 const ListSong = () => {
-  return (
-    <div>ListSong</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default ListSong
+  const fetchSongs = async () => {
+    try {
+      const response = await axios.get(`${url}/api/song/list`);
+      console.log('Response:', response.data);
+
+      if (response.data.success) {
+        setData(response.data.songs);
+        console.log('Songs:', response.data.songs);
+      } else {
+        toast.error("Failed to fetch songs");
+      }
+    } catch (error) {
+      toast.error("Error occurred");
+      console.error('Fetch error:', error);
+    }
+  };
+
+  const removeSong = async (id)=>{
+    try {
+
+        const response = await axios.post(`${url}/api/song/remove`,{id});
+
+        if (response.data.success) {
+            toast.success(response.data.message);
+            await fetchSongs();
+            
+        }
+        
+    } catch (error) {
+        toast.error("Error occured")
+        
+    }
+
+  }
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  useEffect(() => {
+    console.log('Data state updated:', data);
+  }, [data]);
+
+  return (
+    <div>
+      <p>All Songs List</p>
+      <br />
+      <div>
+        <div style={{ display: 'grid', gridTemplateColumns: '0.5fr 1fr 2fr 1fr 0.5fr', alignItems: 'center', gap: '2.5px', padding: '3px', border: '1px solid gray', backgroundColor: 'lightgray' }}>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Album</b>
+          <b>Duration</b>
+          <b>Action</b>
+        </div>
+        {data.map((item, index) => (
+          <div key={index} style={{ display: 'grid', gridTemplateColumns: '0.5fr 1fr 2fr 1fr 0.5fr', alignItems: 'center', gap: '2.5px', padding: '3px', border: '1px solid gray' }}>
+            <img style={{ width: '50px' }} src={item.image} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.album}</p>
+            <p>{item.duration}</p>
+            <p className='cursor-pointer' onClick={()=>removeSong(item._id)}>x</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ListSong;
